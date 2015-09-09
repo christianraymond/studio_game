da8694a26e0d7c9a1c5efa44f65e7b8061bdf3a1
christianraymond <Ombenisn.raymond@gmail.com>
2015-09-09 12:34:11 +0200

    separate files

---
+++ player.rb
@@ -0,0 +1,42 @@
+
+class Player
+  attr_accessor :name, :health
+  def initialize(name, health=100)
+    @name = name.capitalize
+    @health = health
+  end
+
+  def score
+    @health + @name.length
+  end
+
+  def name=(new_name)
+    @name = new_name.upcase
+  end
+
+  def to_s
+    "I'm #{@name} with a health of #{@health} and score of #{score}"
+  end
+
+  def blam
+    @health += 10
+    puts "I'm #{@name.upcase} and got blamed"
+  end
+
+  def w00t
+    @health -= 15
+    puts "I'm #{@name.upcase} and got wooted!"
+  end
+  
+end
+
+if __FILE__ == $0
+
+player = Player.new("moe")
+puts player.name
+puts player.health
+player.w00t
+puts player.health
+player.blam
+puts player.health
+end
